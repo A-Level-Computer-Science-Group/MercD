3cc9e7b719406ead664dffbf8edc62868e52c9c6
ArmanSattar <Armanscsgo@gmail.com>
2019-12-16 20:14:10 +0000

    Added position and physics class

---
+++ Physics.cs
@@ -0,0 +1,11 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace main
+{
+    class Physics
+    {
+        public Vector2 speed{get;set;}
+    }
+}
---
+++ Position.cs
@@ -0,0 +1,12 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace main
+{
+    class Position
+    {
+        public float xPos {get;set;}
+        public float yPos {get;set;}
+    }
+}
