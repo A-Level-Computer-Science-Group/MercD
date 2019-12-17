217388ec2199a75549e82ea46563b8de973fdd09
ArmanSattar <Armanscsgo@gmail.com>
2019-12-17 17:57:12 +0000

    Initialization of Camera

---
+++ Camera.cs
@@ -0,0 +1,12 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Camera
+{
+    public class Camera
+    {
+
+    }
+}
