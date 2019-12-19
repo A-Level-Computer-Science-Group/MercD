fc99592874ed30a837ede54a26fab0d000fd7654
ArmanSattar <Armanscsgo@gmail.com>
2019-12-19 19:10:06 +0000

    idk

--- CameraRelative.cs
+++ CameraRelative.cs
@@ -6,6 +6,7 @@ namespace Camera
     {
         private Camera camera;
         private SpriteBatch spriteBatch;
+        private Vector2 positionDifference;
 
         public CameraRelative(Camera camera)
         {
@@ -14,12 +15,14 @@ namespace Camera
 
         public void Draw(Texture2D texture, Rectangle destinationRectangle)
         {
+            positionDifference.X = camera.scope.X - destinationRectangle.X;
+            positionDifference.Y = camera.scope.Y - destinationRectangle.Y;
+            spriteBatch.Begin();
             if (destinationRectangle.Intersects(camera.scope))
             {
-                spriteBatch.Begin();
                 spriteBatch.Draw(texture, destinationRectangle, Color.Black);
-                spriteBatch.End();
             }
+            spriteBatch.End();
         }
     }
 }
