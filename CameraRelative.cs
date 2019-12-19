55df8aee101a69a5b842e9996ead343879ba5c08
ArmanSattar <Armanscsgo@gmail.com>
2019-12-19 19:27:05 +0000

    Fixed another issue

--- CameraRelative.cs
+++ CameraRelative.cs
@@ -5,22 +5,20 @@ namespace Camera
     class CameraRelative
     {
         private Camera camera;
-        private SpriteBatch spriteBatch;
-
         public CameraRelative(Camera camera)
         {
             this.camera = camera;
         }
 
-        public void Draw(Texture2D texture, Rectangle destinationRectangle)
+        public void Draw(Texture2D texture, Rectangle destinationRectangle, SpriteBatch spriteBatch)
         {
-            Rectangle positionDifference = destinationRectangle; 
-            positionDifference.X = camera.scope.X - destinationRectangle.X;
-            positionDifference.Y = camera.scope.Y - destinationRectangle.Y;
+            Rectangle cameraRelativePosition = destinationRectangle; 
+            cameraRelativePosition.X = camera.scope.X - destinationRectangle.X;
+            cameraRelativePosition.Y = camera.scope.Y - destinationRectangle.Y;
             spriteBatch.Begin();
             if (destinationRectangle.Intersects(camera.scope))
             {
-                spriteBatch.Draw(texture, positionDifference, Color.Black);
+                spriteBatch.Draw(texture, cameraRelativePosition, Color.Black);
             }
             spriteBatch.End();
         }
