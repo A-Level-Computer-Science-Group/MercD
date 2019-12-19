16592fe1ada138c58d7d84dc3f1d1d2f92e7a162
ArmanSattar <Armanscsgo@gmail.com>
2019-12-19 13:04:14 +0000

    Added constructor which makes a rectangle

--- Camera.cs
+++ Camera.cs
@@ -1,7 +1,17 @@
+using Microsoft.Xna.Framework;
 namespace Camera
 {
     public class Camera
     {
-        
+        private int width;
+        private int height;
+
+        public Camera(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            Rectangle rectangle = new Rectangle(0, 0, width, height);
+        }
     }
 }
