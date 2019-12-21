8bb1db473267630ac1d3d16ffd58a9c8864b249a
ArmanSattar <Armanscsgo@gmail.com>
2019-12-21 15:20:09 +0000

    Added Zoom with a bug

--- Camera.cs
+++ Camera.cs
@@ -1,37 +1,45 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Input;
-namespace Camera
-{
-    public class Camera
-    {
-        public Rectangle scope;
-        public Camera(Rectangle scope)
+using Microsoft.Xna.Framework; 
+using Microsoft.Xna.Framework.Input; 
+namespace Camera 
+{ 
+    public class Camera 
+    { 
+        private int perviousScrollWheelValue = 0;
+        public float zoom = 1f;
+        public Rectangle scope; 
+        public Camera(Rectangle scope) 
+        { 
+            this.scope = scope; 
+        } 
+        public void Update(GameTime gameTime) 
         {
-            this.scope = scope;
-        }
-
-        public void Update(GameTime gameTime)
-        {
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            MouseState currentMouseState = Mouse.GetState();
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) 
+            { 
+                scope.Y -= 5; 
+            } 
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) 
+            { 
+                scope.Y += 5; 
+            } 
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) 
+            { 
+                scope.X -= 5; 
+            } 
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) 
+            { 
+                scope.X += 5; 
+            } 
+            if (currentMouseState.ScrollWheelValue < perviousScrollWheelValue)
             {
-                scope.Y -= 5;
+                zoom -= 0.1f;
+                perviousScrollWheelValue = currentMouseState.ScrollWheelValue;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            if (currentMouseState.ScrollWheelValue > perviousScrollWheelValue)
             {
-                scope.Y += 5;
+                zoom = 0.1f;
+                perviousScrollWheelValue = currentMouseState.ScrollWheelValue;
             }
-            if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                scope.X -= 5;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                scope.X += 5;
-            }
-            if (Keyboard.GetState().IsKeyDown(Keys.Scroll))
-            {
-
-            }
-        }
-    }
+        } 
+    } 
 }
--- CameraRelative.cs
+++ CameraRelative.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input; 
 using main;
 using System;
 namespace Camera
@@ -7,6 +8,8 @@ namespace Camera
     class CameraRelative
     {
         private Camera camera;
+        private GameTime gameTime;
+        MouseState currentMouseState = Mouse.GetState();
         public CameraRelative(Camera camera)
         {
             this.camera = camera;
@@ -15,13 +18,14 @@ namespace Camera
         {
             float screenScaleRatioX = (float)screenResolution.width / (float)camera.scope.Width;
             float screenScaleRatioY = (float)screenResolution.height / (float)camera.scope.Height;
-
+            camera.Update(gameTime);
             Rectangle cameraRelativeRectangle = new Rectangle(
                 destinationRectangle.X - camera.scope.X,
                 destinationRectangle.Y - camera.scope.Y,
-                (int)(destinationRectangle.Width * screenScaleRatioX),
-                (int)(destinationRectangle.Height * screenScaleRatioY)
+                (int)(destinationRectangle.Width * screenScaleRatioX * camera.zoom),
+                (int)(destinationRectangle.Height * screenScaleRatioY * camera.zoom)
             );
+            Console.Write($"Zoom:{camera.zoom} & Scroll Value:{currentMouseState.ScrollWheelValue}");
             if (destinationRectangle.Intersects(camera.scope))
             {
                 spriteBatch.Draw(texture, cameraRelativeRectangle, Color.White);
