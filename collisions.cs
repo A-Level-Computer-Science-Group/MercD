6dc87294ea5edb5e64d9853b532ac99f38001b13
Mehran <mehransattarzadeh@gmail.com>
2020-01-20 22:18:48 +0000

    Removed using things

--- collisions.cs
+++ collisions.cs
@@ -1,11 +1,4 @@
-using System;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace main
 {
