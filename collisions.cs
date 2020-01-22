a2b65f3b0c9b91835a0e91416a11dc16495d241f
Mehran <mehransattarzadeh@gmail.com>
2020-01-22 13:59:27 +0000

    Removed whitespace

--- collisions.cs
+++ collisions.cs
@@ -4,17 +4,14 @@ namespace main
 {
  public static class intersection
  {
-    
      static bool intersect(Rectangle first, Rectangle second) =>
          ((first.Bottom < second.Bottom)&&(first.Bottom < second.Top)
          ||((second.Bottom < first.Top)&&(second.Bottom > first.Bottom))
          ||((first.Top < second.Top)&&(first.Bottom > second.Bottom))
-         ||((second.Top > first.Bottom)&&(first.Top > second.Top))
-         &&
+         ||((second.Top > first.Bottom)&&(first.Top > second.Top))&&
          ((first.Right > second.Left)&& first.Right< second.Right)
          ||((first.Left > second.Left)&& (first.Left < second.Left))
          ||((second.Right < first.Right)&&(second.Right > first.Left))
          ||((second.Left > first.Left)&&(second.Left < first.Right)));
-      
  }   
 }
