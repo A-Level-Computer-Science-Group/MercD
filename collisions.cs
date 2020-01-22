11219289ef55f4856d0454a2723ddcd9b2022234
Mehran <mehransattarzadeh@gmail.com>
2020-01-22 13:44:50 +0000

    Implemented intersection function

--- collisions.cs
+++ collisions.cs
@@ -1,13 +1,20 @@
-using System;
+using Microsoft.Xna.Framework;
 
 namespace main
 {
  public static class intersection
  {
     
-     Bool intersect(Rect first, Rect second) {
-         
-      }
-
+     static bool intersect(Rectangle first, Rectangle second) =>
+         ((first.Bottom < second.Bottom)&&(first.Bottom < second.Top)
+         ||((second.Bottom < first.Top)&&(second.Bottom > first.Bottom))
+         ||((first.Top < second.Top)&&(first.Bottom > second.Bottom))
+         ||((second.Top > first.Bottom)&&(first.Top > second.Top))
+         &&
+         ((first.Right > second.Left)&& first.Right< second.Right)
+         ||((first.Left > second.Left)&& (first.Left < second.Left))
+         ||((second.Right < first.Right)&&(second.Right > first.Left))
+         ||((second.Left > first.Left)&&(second.Left < first.Right)));
+      
  }   
 }
