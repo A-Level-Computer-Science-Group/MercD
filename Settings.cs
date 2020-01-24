9b9bae04366f4ed41c0aace87562bd55696675d2
Mehran <mehransattarzadeh@gmail.com>
2020-01-24 19:44:22 +0000

    Fixed shadowing issue

--- Settings.cs
+++ Settings.cs
@@ -7,14 +7,14 @@ namespace main
     {
         public bool fullScreen;
         public string playerName;
-        public string musicVolume;
-        public string masterVolume;
-        public string forward;
-        public string backward;
-        public string right;
-        public string left;
+        public int musicVolume;
+        public int masterVolume;
+        public char forward;
+        public char backward;
+        public char right;
+        public char left;
         public string jump;
-        public string interact;
+        public char interact;
         
         public void readFile()
         {
@@ -22,23 +22,23 @@ namespace main
             var parser = new FileIniDataParser();
             IniData data = parser.ReadFile("Settings.ini");
             string useFullScreenStr = data["General"]["fullscreen"];
-            bool fullScreen = bool.Parse(useFullScreenStr);
+            fullScreen = bool.Parse(useFullScreenStr);
             string playerName = data["General"]["PlayerName"];
             string useMusicVolumeInt = data["Audio"]["MusicVolume"];
-            int musicVolume = int.Parse(useMusicVolumeInt);
+            musicVolume = int.Parse(useMusicVolumeInt);
             string useMasterVolumeInt = data["Audio"]["MasterVolume"];
-            int masterVolume = int.Parse(useMasterVolumeInt);
+            masterVolume = int.Parse(useMasterVolumeInt);
             string useForwardChar = data["Controls"]["Forward"];
-            char forward = char.Parse(useForwardChar);
+            forward = char.Parse(useForwardChar);
             string useBackwardChar = data["Controls"]["Backward"];
-            char backward = char.Parse(useBackwardChar);
+            backward = char.Parse(useBackwardChar);
             string useLeftChar = data["Controls"]["Left"];
-            char left = char.Parse(useLeftChar);
+            left = char.Parse(useLeftChar);
             string useRightChar = data["Controls"]["Right"];
-            char right = char.Parse(useRightChar);
+            right = char.Parse(useRightChar);
             string jump = data["Controls"]["Jump"];
             string useInteractChar = data["Controls"]["Interact"];
-            char interact = char.Parse(useInteractChar);
+            interact = char.Parse(useInteractChar);
         
         }  
         
