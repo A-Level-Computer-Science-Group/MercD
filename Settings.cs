ef3fecba385bff62012f3d11cec67a82112b7f47
Mehran <mehransattarzadeh@gmail.com>
2020-01-24 22:35:21 +0000

    Revert "Added screen dimension writing method" as there is a mistake in commit
    This reverts commit c272d74d7f1b72c46e43662df42cd5cd45b2311a.

--- Settings.cs
+++ Settings.cs
@@ -6,13 +6,13 @@ namespace main
     public class Settings
     {
         public bool consoleDebugScreenDimensions;
-
+        
         public Settings()
         {
             var parser = new FileIniDataParser();
             IniData data = parser.ReadFile("Settings.ini");
             consoleDebugScreenDimensions = bool.Parse(data["ConsoleDebug"]["screen_dimensions"]);
-        }
-
+        }  
+        
     }
 }
